ecb159d1812e136702c695d4827be2eec489595c
AngelMaldonado <amaldonadojuarez@gmail.com>
2022-08-04 13:19:08 -0500

    🍏01Intro - Fin de archivos y streams en js

--- Node/01Intro/03files.js
+++ Node/01Intro/03files.js
@@ -14,14 +14,22 @@ const fs = require('fs');
 // });
 
 // directories
-if(!fs.existsSync('./assets')) {
-    fs.mkdir('./assets', (err) => {
+// if(!fs.existsSync('./assets')) {
+//     fs.mkdir('./assets', (err) => {
+//         if(err) console.log(err);
+//         console.log('Folder created!');
+//     });
+// } else {
+//     fs.rmdir('./assets', (err) => {
+//        if(err) console.log(err);
+//        console.log('Folder deleted!');
+//     });
+// }
+
+// deleting files
+if(fs.existsSync('./docs/deleteme.txt')) {
+    fs.unlink('./docs/deleteme.txt', (err) => {
         if(err) console.log(err);
-        console.log('Folder created!');
-    });
-} else {
-    fs.rmdir('./assets', (err) => {
-       if(err) console.log(err);
-       console.log('Folder deleted!');
+        console.log('File deleted!');
     });
 }
