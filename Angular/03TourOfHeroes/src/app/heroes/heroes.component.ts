d45a939903c203c8634d08cad644edf157a95f77
AngelMaldonado <amaldonadojuarez@gmail.com>
2022-08-08 15:13:46 -0500

    🔴03TourOfHeroes - Servicio de mensajes y cambio de fetch asíncrono para la lista de héroes

--- Angular/03TourOfHeroes/src/app/heroes/heroes.component.ts
+++ Angular/03TourOfHeroes/src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -12,17 +13,18 @@ export class HeroesComponent implements OnInit {
   heroes: Hero[] = [];
 
   // dependency injection:
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
   getHeroes(): void {
-    this.heroes = this.heroService.getHeroes();
+    this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 }
